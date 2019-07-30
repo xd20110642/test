384589261303de28f9234c95d685c5a5fb246386
xd20110642 <790057064@qq.com>
2019-07-30 22:45:06 +0800

    尝试分支合并

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const root=require('./routers/root')
 app.set('port', 4000);
 app.set('rootURL','http://www.baidu.com')
 app.use('/',root);
-
+console.log("12312312312")
 app.listen(app.get('port'), (req,res,) => {
     console.log('Example app listening on port port!');
 });
